13973ad5240b77767577218a4dfe2895b943df6f
Axo <109083646+Ax0-0@users.noreply.github.com>
2026-06-04 14:03:38 +0200

    fix: handle None screen in data.py when no display available

--- config/data.py
+++ config/data.py
@@ -19,8 +19,12 @@ HOME_DIR = os.path.expanduser("~")
 CONFIG_DIR = os.path.expanduser(f"~/.config/{APP_NAME_CAP}")
 
 screen = Gdk.Screen.get_default()
-CURRENT_WIDTH = screen.get_width()
-CURRENT_HEIGHT = screen.get_height()
+if screen is not None:
+    CURRENT_WIDTH = screen.get_width()
+    CURRENT_HEIGHT = screen.get_height()
+else:
+    CURRENT_WIDTH = 1920
+    CURRENT_HEIGHT = 1080
 
 CONFIG_FILE = get_relative_path("../config/config.json")
 MATUGEN_STATE_FILE = os.path.join(CONFIG_DIR, "matugen")
